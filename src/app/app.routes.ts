f25836d6c6f69f338410c65a625c45adde3f2592
VikaNaumenko <67087076+VikaNaumenko@users.noreply.github.com>
2023-12-25 19:48:47 +0100

    add personal cabinet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PersonalCabinetMain } from './personal.cabinet/personal.cabinet.component';
+import { MainPage } from './main.page/main.page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: MainPage},
+    // { path: 'main-page', component: MainPage},  
+    { path: 'personal-cabinet-main', component: PersonalCabinetMain },  
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { PersonalCabinetMain } from '../personal.cabinet/personal.cabinet.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
+
 export class Header {
 }
