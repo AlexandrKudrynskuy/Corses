a097084a0bb55a9191f86df82e47f8485d499b1e
VikaNaumenko <67087076+VikaNaumenko@users.noreply.github.com>
2023-12-26 21:22:42 +0100

    add bootstrap

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,7 @@ import { provideRouter, Routes } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { PersonalCabinetMain } from './personal.cabinet/personal.cabinet.component';
-const appRoutes: Routes =[
-  // { path: "personal-cabinet-main", component: PersonalCabinetMain},
-  { path: '', redirectTo: '/personal-cabinet-main', pathMatch: 'full' },   
-  { path: 'personal-cabinet-main', component: PersonalCabinetMain },  
-];
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()]
 };
